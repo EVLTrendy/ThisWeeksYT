1b1f88301b75ab091022e5dfd0312b18bd46c274
Marc Cannon <marcdoess@icloud.com>
2023-08-04 18:44:42 -0400

    Updated script, index, and styles

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const apiKey = 'AIzaSyAfq3GCaHig-hg5L1emI5JnbkOGkZiXDiI';
+// script.js
+const apiKey = 'AIzaSyAfq3GCaHig-hg5L1emI5JnbkOGkZiXDiI'; // Replace with your actual API key
 const videoContainer = document.getElementById('videos');
 
 async function fetchTrendingVideos() {
@@ -8,12 +9,20 @@ async function fetchTrendingVideos() {
   data.items.forEach(item => {
     const videoTitle = item.snippet.title;
     const videoId = item.id;
+
+    const videoBox = document.createElement('div');
+    videoBox.classList.add('video-box');
     
-    const videoLink = document.createElement('a');
-    videoLink.href = `https://www.youtube.com/watch?v=${videoId}`;
-    videoLink.textContent = videoTitle;
+    const videoIframe = document.createElement('iframe');
+    videoIframe.src = `https://www.youtube.com/embed/${videoId}`;
+    videoIframe.width = '100%';
+    videoIframe.height = '315';
+    videoIframe.frameBorder = '0';
+    videoIframe.allowFullscreen = true;
     
-    videoContainer.appendChild(videoLink);
+    videoBox.appendChild(videoIframe);
+    
+    videoContainer.appendChild(videoBox);
   });
 }
 
