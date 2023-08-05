d91125da0016df54fc96d0a9ddfe272f13a01e39
Marc Cannon <marcdoess@icloud.com>
2023-08-05 12:39:54 -0400

    Updated the files

--- script.js
+++ script.js
@@ -1,34 +1,48 @@
-// script.js
-const apiKey = 'AIzaSyAfq3GCaHig-hg5L1emI5JnbkOGkZiXDiI'; // Replace with your actual API key
-const videoContainer = document.getElementById('videos');
+async function getTrendingVideos() {
+    const apiKey = 'AIzaSyAfq3GCaHig-hg5L1emI5JnbkOGkZiXDiI'; // Replace with your YouTube Data API key
 
-async function fetchTrendingVideos() {
-  const excludedCategories = ['10', '17', '20']; // Music, Sports, Gaming
-  const excludedCategoryString = excludedCategories.join(',');
+    // Get the video category IDs to exclude
+    const excludedCategories = ['10', '17', '44', '1']; // music, sports, trailers, movies
 
-  const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=10&key=${apiKey}&videoCategoryId=${excludedCategoryString}`);
-  const data = await response.json();
-  
-  data.items.forEach(item => {
-    const videoTitle = item.snippet.title;
-    const videoId = item.id;
-    
-    const videoBox = document.createElement('div');
-    videoBox.className = 'video-box';
+    // Fetch video categories...
+    const categoriesResponse = await fetch(`https://www.googleapis.com/youtube/v3/videoCategories?part=snippet&regionCode=US&key=${apiKey}`);
+    const categoriesData = await categoriesResponse.json();
 
-    const videoTitleElement = document.createElement('div');
-    videoTitleElement.className = 'video-title';
-    videoTitleElement.textContent = videoTitle;
-    videoBox.appendChild(videoTitleElement);
+    const excludedCategoryIds = categoriesData.items
+        .filter(category => excludedCategories.includes(category.id))
+        .map(category => category.id);
 
-    const videoPreview = document.createElement('iframe');
-    videoPreview.src = `https://www.youtube.com/embed/${videoId}`;
-    videoPreview.className = 'video-preview';
-    videoPreview.allowFullscreen = true;
-    videoBox.appendChild(videoPreview);
-    
-    videoContainer.appendChild(videoBox);
-  });
+    // Fetch trending videos...
+    const trendingResponse = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=50&regionCode=US&key=${apiKey}`);
+    const trendingData = await trendingResponse.json();
+
+    // Filter out videos from excluded categories...
+    const filteredVideos = trendingData.items.filter(video => !excludedCategoryIds.includes(video.snippet.categoryId));
+
+    // Get the top 7 videos from the filtered list...
+    const top7Videos = filteredVideos.slice(0, 7);
+
+    // Display video boxes...
+    const videoContainer = document.getElementById('video-container');
+    videoContainer.innerHTML = ''; // Clear previous content
+    top7Videos.forEach(video => {
+        const videoBox = document.createElement('div');
+        videoBox.className = 'video-box';
+
+        const videoThumbnail = document.createElement('img');
+        videoThumbnail.className = 'video-thumbnail';
+        videoThumbnail.src = video.snippet.thumbnails.default.url;
+
+        const videoTitle = document.createElement('p');
+        videoTitle.className = 'video-title';
+        videoTitle.textContent = video.snippet.title;
+
+        videoBox.appendChild(videoThumbnail);
+        videoBox.appendChild(videoTitle);
+
+        videoContainer.appendChild(videoBox);
+    });
 }
 
-fetchTrendingVideos();
+// Call the function when the page loads
+document.addEventListener('DOMContentLoaded', getTrendingVideos);
