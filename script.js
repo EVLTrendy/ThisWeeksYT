482fc006b7233d76ce700598b48c7b442a34d676
EVLTrendy <141459487+EVLTrendy@users.noreply.github.com>
2023-08-05 13:08:56 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 async function getTrendingVideos() {
-    const apiKey = 'AIzaSyAfq3GCaHig-hg5L1emI5JnbkOGkZiXDiI'; // Replace with your YouTube Data API key
+    const apiKey = 'AIzaSyAfq3GCaHig-hg5L1emI5JnbkOGkZiXDiI'; 
 
-    // Get the video category IDs to exclude
-    const excludedCategories = ['10', '17', '44', '1']; // music, sports, trailers, movies
+    
+    const excludedCategories = ['10', '17', '44', '1']; 
 
-    // Fetch video categories...
+    
     const categoriesResponse = await fetch(`https://www.googleapis.com/youtube/v3/videoCategories?part=snippet&regionCode=US&key=${apiKey}`);
     const categoriesData = await categoriesResponse.json();
 
@@ -12,19 +12,17 @@ async function getTrendingVideos() {
         .filter(category => excludedCategories.includes(category.id))
         .map(category => category.id);
 
-    // Fetch trending videos...
+  
     const trendingResponse = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=50&regionCode=US&key=${apiKey}`);
     const trendingData = await trendingResponse.json();
 
-    // Filter out videos from excluded categories...
+
     const filteredVideos = trendingData.items.filter(video => !excludedCategoryIds.includes(video.snippet.categoryId));
 
-    // Get the top 7 videos from the filtered list...
     const top7Videos = filteredVideos.slice(0, 7);
 
-    // Display video boxes...
     const videoContainer = document.getElementById('video-container');
-    videoContainer.innerHTML = ''; // Clear previous content
+    videoContainer.innerHTML = ''; 
     top7Videos.forEach(video => {
         const videoBox = document.createElement('div');
         videoBox.className = 'video-box';
@@ -33,16 +31,17 @@ async function getTrendingVideos() {
         videoThumbnail.className = 'video-thumbnail';
         videoThumbnail.src = video.snippet.thumbnails.default.url;
 
-        const videoTitle = document.createElement('p');
-        videoTitle.className = 'video-title';
-        videoTitle.textContent = video.snippet.title;
+        const videoTitleLink = document.createElement('a');
+        videoTitleLink.className = 'video-title';
+        videoTitleLink.textContent = video.snippet.title;
+        videoTitleLink.href = `https://www.youtube.com/watch?v=${video.id}`;
 
         videoBox.appendChild(videoThumbnail);
-        videoBox.appendChild(videoTitle);
+        videoBox.appendChild(videoTitleLink);
 
         videoContainer.appendChild(videoBox);
     });
 }
 
-// Call the function when the page loads
+
 document.addEventListener('DOMContentLoaded', getTrendingVideos);
